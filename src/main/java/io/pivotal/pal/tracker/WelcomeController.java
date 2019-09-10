8d5aa621bea73eef3aeb2cf3572451ea399fdc64
Tim Whitlock <twhitlock@kr.af.mil>
2019-09-10 16:57:41 -0600

    Wendy and Colby day 2 added environment to variable

--- src/main/java/io/pivotal/pal/tracker/WelcomeController.java
+++ src/main/java/io/pivotal/pal/tracker/WelcomeController.java
@@ -1,13 +1,31 @@
 package io.pivotal.pal.tracker;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class WelcomeController {
 
+    public WelcomeController(@Value("${welcome.message}")String msg) {
+        this.message = msg;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    private String message = "Wendy/Colby are's awesome";
+
+
+
+
     @GetMapping("/")
     public String sayHello() {
-        return "Wendy/Colby are's awesome";
+        return message;
     }
 }
